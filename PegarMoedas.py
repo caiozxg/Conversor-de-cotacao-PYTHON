c2f4467489c5cd60ae0c972c53d7ab65e59d86ad
caiozxg <105797281+caiozxg@users.noreply.github.com>
2024-06-11 18:00:23 -0300

    PegarMoedas.py

--- PegarMoedas.py
+++ PegarMoedas.py
@@ -8,8 +8,18 @@ def nome_moedas():
         
     nomesmoedas = dic_moedas["xml"]
     return nomesmoedas
+def conversoesdisponiveis():
+    with open("ConversoesDisponiveis.xml","rb") as conversoes_moedas:
+        dic_conversoes = xmltodict.parse(conversoes_moedas)
 
-with open("ConversoesDisponiveis.xml","rb") as conversoes_moedas:
-    dic_conversoes = xmltodict.parse(conversoes_moedas)
-
-    conversoes = dic_conversoes["xml"]
+        conversoes = dic_conversoes["xml"]
+        #criamos um dicionario e vamos testar se é possivel fazer a transformação
+        dic_testeconversoesdisponiveis = {}
+        for pardemoedas in conversoes:
+            moedasantes, moedasdepois = pardemoedas.split('-')
+            if moedasantes in dic_testeconversoesdisponiveis:
+                dic_testeconversoesdisponiveis[moedasantes].append(moedasdepois)
+            else:
+                dic_testeconversoesdisponiveis[moedasantes] = [moedasdepois]
+    return dic_testeconversoesdisponiveis
+    
